b96172e6f8cdbabd534660fc87e64743be669516
Ondrej Fikar <ondrej.fikar@nic.cz>
2021-08-16 11:26:44 +0200

    Issue #22 - Receive Greeting automatically and store it

--- epplib/tests/test_client.py
+++ epplib/tests/test_client.py
@@ -16,20 +16,24 @@
 # You should have received a copy of the GNU General Public License
 # along with FRED.  If not, see <https://www.gnu.org/licenses/>.
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Any, Dict, Type, cast
 from unittest import TestCase
-from unittest.mock import Mock, call, sentinel
+from unittest.mock import Mock, call
 
 from lxml.etree import Element, XMLSchema
 
 from epplib.client import Client
 from epplib.commands import Request
-from epplib.responses import Response
+from epplib.responses import Greeting, Response
 from epplib.transport import Transport
+
+GREETING = (Path(__file__).parent / 'data/greeting_template.xml').read_bytes().replace(b'{expiry}', b'')
 
 
 class DummyTransport(Transport):
 
+    greeting_sent = False
     raw_response = b'This is the Response!'
 
     def connect(self) -> None:
@@ -42,6 +46,9 @@ class DummyTransport(Transport):
         pass
 
     def receive(self) -> bytes:
+        if not self.greeting_sent:
+            self.greeting_sent = True
+            return GREETING
         return self.raw_response
 
 
@@ -80,6 +87,7 @@ class TestClient(TestCase):
 
     def test_context_manager(self):
         transport = Mock(spec=Transport)
+        transport.receive.return_value = GREETING
         client = Client(transport)
 
         with client as cl:
@@ -87,42 +95,53 @@ class TestClient(TestCase):
 
         self.assertEqual(client, cl)
         self.assertEqual(
-            [call.connect(), call.send(DummyRequest.raw_request), call.receive(), call.close()],
+            [call.connect(), call.receive(), call.send(DummyRequest.raw_request), call.receive(), call.close()],
             transport.mock_calls
         )
 
     def test_connect(self):
         transport = Mock(spec=Transport)
-        Client(transport).connect()
-        transport.connect.assert_called_once_with()
+        transport.receive.return_value = GREETING
+
+        client = Client(transport)
+        client.connect()
+        self.assertEqual(transport.mock_calls, [call.connect(), call.receive()])
+        self.assertIsInstance(client.greeting, Greeting)
+        self.assertEqual(client.greeting.sv_id, 'EPP server (DSDng)')  # type: ignore
 
     def test_close(self):
         transport = Mock(spec=Transport)
+        transport.receive.return_value = GREETING
+
         client = Client(transport)
 
         client.connect()
         client.close()
-        transport.close.assert_called_once_with()
+        self.assertEqual(transport.mock_calls, [call.connect(), call.receive(), call.close()])
 
     def test_receive(self):
-        client = Client(DummyTransport(), sentinel.schema)
+        mock_schema = Mock(spec=XMLSchema)
+        mock_schema.assertValid = Mock()  # Otherwise we get AttributeError: Attributes cannot start with 'assert'
+        client = Client(DummyTransport(), mock_schema)
 
         with client:
-            response = client.receive(DummyResponse)
+            response = client._receive(DummyResponse)
 
         self.assertEqual(type(response), DummyResponse)
         self.assertEqual(cast(DummyResponse, response).raw_response, DummyTransport.raw_response)
-        self.assertEqual(cast(DummyResponse, response).schema, sentinel.schema)
+        self.assertEqual(cast(DummyResponse, response).schema, mock_schema)
 
     def test_send(self):
         transport = Mock(wraps=DummyTransport())
-        client = Client(transport, sentinel.schema)
+        mock_schema = Mock(spec=XMLSchema)
+        mock_schema.assertValid = Mock()  # Otherwise we get AttributeError: Attributes cannot start with 'assert'
+        client = Client(transport, mock_schema)
 
         with client:
             request = DummyRequest()
             response = client.send(request)
 
-        self.assertEqual(request.schema, sentinel.schema)
+        self.assertEqual(request.schema, mock_schema)
 
         transport.send.assert_called_with(DummyRequest.raw_request)
 
