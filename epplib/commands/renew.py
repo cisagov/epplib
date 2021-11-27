78275d5af71085199cd97f3ea8e18a840394fb5e
Ondrej Fikar <ondrej.fikar@nic.cz>
2021-11-27 15:02:04 +0100

    Fix #73 - Fix module docstrings

--- epplib/commands/renew.py
+++ epplib/commands/renew.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with FRED.  If not, see <https://www.gnu.org/licenses/>.
 
-"""Module providing EPP commands."""
+"""Module providing EPP renew command."""
 from dataclasses import dataclass
 from datetime import date
 from typing import Optional
