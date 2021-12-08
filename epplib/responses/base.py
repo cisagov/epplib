bb7e9bae1420e589f83bca5110b8c0e2341f416c
Ondrej Fikar <ondrej.fikar@nic.cz>
2021-12-08 11:21:59 +0100

    Fix #94 - Fix deprecation warning

--- epplib/responses/base.py
+++ epplib/responses/base.py
@@ -315,7 +315,7 @@ class Result(Response, Generic[T]):
             for child in element:
                 extension_class = EXTENSIONS.get(child.tag, None)
                 if extension_class is None:
-                    LOGGER.warn('Could not find class to extract extension {}.'.format(child.tag))
+                    LOGGER.warning('Could not find class to extract extension {}.'.format(child.tag))
                 else:
                     data.append(extension_class.extract(child))
         return data
