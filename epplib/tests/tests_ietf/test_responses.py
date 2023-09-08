9a748844ab996e560af8567085f9f279ebaadf73
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2023-09-08 00:47:46 -0600

    Additional Test cases
    Modified the existing test case for better coverage and added a test case which tests for the negative (no contact)

--- epplib/tests/tests_ietf/test_responses.py
+++ epplib/tests/tests_ietf/test_responses.py
@@ -61,10 +61,54 @@ class TestInfoDomainResult(TestCase):
         print("\n\n\n")
         print(f"expected: {expected[0].contacts}")
         print(f"result: {result.res_data[0].contacts}")
-        # Both objects are the same        
+        # Both objects are the same...        
         self.assertEqual(result.res_data, expected)
-        # Both objects have the same contacts
-        self.assertEqual(expected[0].contacts, result.res_data[0].contacts)
+        # Both objects have the same contacts...
+        self.assertEqual(result.res_data[0].contacts, expected[0].contacts)
+        # Manually checks for contacts on the returned object (Tests for optional vars)...
+        text_to_check = "DomainContact(contact='CONT2', type='security')"
+        self.assertIn(text_to_check, str(result.res_data[0]))
+        text_to_check = "DomainContact(contact='CONT3', type='tech')"
+        self.assertIn(text_to_check, str(result.res_data[0]))
+
+    def test_parse_minimal_no_contact(self):
+        location= Path(__file__).parent / "data" / "infoDomainNoContact.xml"
+        print(location)
+        xml = (location).read_bytes()
+        print(xml)
+        result = InfoDomainResult.parse(xml, SCHEMA)
+        expected = [
+            InfoDomainResultData(
+                name="test3.gov",
+                roid="DF1340360-GOV",
+                statuses=[Status("serverTransferProhibited", None, None),Status("inactive", None, None)],
+                cl_id="gov2023-ote",
+                registrant="TuaWnx9hnm84GCSU",
+                admins=[],
+                nsset=None,
+                keyset=None,
+                cr_id="gov2023-ote",
+                cr_date=datetime(2023, 8, 15, 23, 56, 36, tzinfo=tzutc()),
+                up_id="gov2023-ote",
+                up_date=datetime(2023, 8, 17, 2, 3, 19, tzinfo=tzutc()),
+                ex_date=date(2024, 8, 15),
+                tr_date=None,
+                auth_info=DomainAuthInfo(pw="2fooBAR123fooBaz")
+            )
+        ]
+        self.assertEqual(result.code, 1000)
+        print("result.res_data")
+        print(result.res_data)
+        print("\n\n\n")
+        print(f"expected: {expected[0].contacts}")
+        print(f"result: {result.res_data[0].contacts}")
+        # Both objects are the same...        
+        self.assertEqual(result.res_data, expected)
+        # Both objects have the same contacts...
+        self.assertEqual(result.res_data[0].contacts, expected[0].contacts)
+        # Manually checks for contacts on the returned object (Tests for optional vars)...
+        text_to_check = "contacts=[]"
+        self.assertIn(text_to_check, str(result.res_data[0]))
 
     # def test_parse_full(self):
     #     xml = (path(__file__).parent  / "responses/result_info_domain.xml").read_bytes()
